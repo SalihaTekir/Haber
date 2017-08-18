817dea192cc1faf2b21d9008745ed1280bcdc907
SalihaTekir <saliha.tekir12@hotmail.com>
2017-08-18 10:38:47 +0300

    qwe

---
+++ SeferTasıApp/ST.DAL/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ST.Models.IdentityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.DAL
+{
+   public class MyContext:IdentityDbContext<ApplicationUser>
+    {
+        public MyContext()
+            : base("name=MyCon")
+        {
+           
+           
+        }
+    }
+}
